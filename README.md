b290b1a8323d1e2a0753e82b57741e0225459305
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-23 21:10:58 +0100

    first complete implementation

--- README.md
+++ README.md
@@ -17,10 +17,11 @@ rosrun mpc_dummy mpc_dummy_talker
 Once you run the node, you can post your request to `mpc/open_parameters` as follows:
 
 ```
-rostopic pub /mpc/open_parameters       \
-  mpc_dummy/OptimisationParameters      \
-  "{'parameter':[1.0,200.0],            \
-   'initial_y':[-3.0,12345.0]}" -1
+rostopic pub /mpc/open_parameters              \
+  mpc_dummy/OptimizationParameters             \
+  "{'parameter':[1.0,200.0],                   \
+   'initial_guess':[0.5, 0.1, 0.3, 0.1, -0.6], \
+   'initial_penalty': 100.0}" --once
 ```
 
 The result will be announced on `/mpc/open_solution`:
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,82 +1,113 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
-#include <sstream>
-#include "mpc_dummy/OptimisationResult.h"
-#include "mpc_dummy/OptimisationParameters.h"
-#include <iostream>
+#include "mpc_dummy/OptimizationResult.h"
+#include "mpc_dummy/OptimizationParameters.h"
 #include "rosenbrock_bindings.hpp"
-
-#define MPC_DUMMY_NODE_NAME "mpc_dummy_talker"
-#define MPC_DUMMY_BASE_TOPIC "/mpc"
-#define MPC_DUMMY_SOLUTION_TOPIC "open_solution"
-#define MPC_DUMMY_PARAMS_TOPIC "open_parameters"
-#define MPC_DUMMY_RATE 10
+#include "talker.hpp"
 
 
-class OptimizationEngineManager {
-    private:
-        mpc_dummy::OptimisationParameters params;
-        mpc_dummy::OptimisationResult results;
-        double p[ROSENBROCK_NUM_PARAMETERS] = {0};
-        double u[ROSENBROCK_NUM_DECISION_VARIABLES] = {0};
-        rosenbrockCache *cache;
-        double init_penalty = 15.0;
+namespace mpc_dummy {
+        /**
+	 * Class mpc_dummy::OptimizationEngineManager manages the 
+	 * exchange of data between the input and output topics
+	 * of this node
+ 	 */
+	class OptimizationEngineManager {
 
-    public:
-    OptimizationEngineManager() {
-        cache = rosenbrock_new();
-    }
+	private:
+		mpc_dummy::OptimizationParameters params;
+		mpc_dummy::OptimizationResult results;
+		double p[ROSENBROCK_NUM_PARAMETERS] = {0};
+		double u[ROSENBROCK_NUM_DECISION_VARIABLES] = {0};
+		rosenbrockCache *cache;
+		double init_penalty = MPC_DUMMY_DEFAULT_INITIAL_PENALTY;
 
-    ~OptimizationEngineManager() {
-        rosenbrock_free(cache);
-    }
-
-    /**
-     * updateResults 
-     */
-    void updateResults(rosenbrockSolverStatus& status) 
-    {
-        std::vector<double> sol(u, u+ROSENBROCK_NUM_DECISION_VARIABLES); 
-        results.solution = sol;
-        std::vector<double> y(status.lagrange, status.lagrange+ROSENBROCK_N1);
-        results.lagrange_multipliers = y;
-        results.inner_iterations = status.num_inner_iterations;
-        results.outer_iterations = status.num_outer_iterations;
-        results.norm_fpr = status.last_problem_norm_fpr;
-        results.penalty = status.penalty;
-        results.status = (int) status.exit_status;
-    }
-
-    void mpcReceiveRequestCallback(
-           const mpc_dummy::OptimisationParameters::ConstPtr& msg)
-    {
-        params = *msg;
-    }
-
-    void publishToTopic(ros::Publisher &publisher) 
-    {
-        publisher.publish(results);
-    }
+	    void publishToTopic(ros::Publisher &publisher) 
+	    {
+		publisher.publish(results);
+	    }
 
 
-    void updateInputData() {
-        if (params.parameter.size() > 0) 
-        {
-	    for (int i=0; i<ROSENBROCK_NUM_PARAMETERS; ++i) 
-              p[i] = params.parameter[i];
-        }
+	    /**
+	     * Updates the input data based on the data that are posted
+	     * on /mpc/open_parameters (copies value from topic data to 
+	     * local variables). This method is responsible for parsing 
+	     * the data announced on the input topic.
+	     */
+	    void updateInputData() {
+                init_penalty = (params.initial_penalty > 1.0) 
+	                         ? params.initial_penalty 
+	                         : MPC_DUMMY_DEFAULT_INITIAL_PENALTY;
+		if (params.parameter.size() > 0) 
+		{
+		    for (size_t i=0; i<ROSENBROCK_NUM_PARAMETERS; ++i) 
+		      p[i] = params.parameter[i];
+		}
 
-        if (params.initial_guess.size() == ROSENBROCK_NUM_DECISION_VARIABLES) 
-        {
-	    for (int i=0; i<ROSENBROCK_NUM_DECISION_VARIABLES; ++i) 
-                u[i] = params.initial_guess[i];
-        }
-    }
+		if (params.initial_guess.size() == ROSENBROCK_NUM_DECISION_VARIABLES) 
+		{
+		    for (size_t i=0; i<ROSENBROCK_NUM_DECISION_VARIABLES; ++i) 
+		        u[i] = params.initial_guess[i];
+		}
+	    }
 
-    rosenbrockSolverStatus solve() {
-        return rosenbrock_solve(cache, u, p, 0, &init_penalty);
-    }
-};
+	    /**
+	     * Call OpEn to solve the problem
+	     */
+	    rosenbrockSolverStatus solve() {
+		return rosenbrock_solve(cache, u, p, 0, &init_penalty);
+	    }
+
+	public:
+	    /**
+	     * Constructor of OptimizationEngineManager
+	     */
+	    OptimizationEngineManager() {
+		cache = rosenbrock_new();
+	    }
+
+	    /**
+	     * Destructor of OptimizationEngineManager
+	     */
+	    ~OptimizationEngineManager() {
+		rosenbrock_free(cache);
+	    }
+
+	    /**
+	     * updateResults 
+	     */
+	    void updateResults(rosenbrockSolverStatus& status) 
+	    {
+		std::vector<double> sol(u, u+ROSENBROCK_NUM_DECISION_VARIABLES); 
+		results.solution = sol;
+		std::vector<double> y(status.lagrange, status.lagrange+ROSENBROCK_N1);
+		results.lagrange_multipliers = y;
+		results.inner_iterations = status.num_inner_iterations;
+		results.outer_iterations = status.num_outer_iterations;
+		results.norm_fpr = status.last_problem_norm_fpr;
+		results.penalty = status.penalty;
+		results.status = (int) status.exit_status;
+                results.solve_time_ms = (double)status.solve_time_ns / 1000000.0;
+	        results.infeasibility_f2 = status.f2_norm;
+	        results.infeasibility_f1 = status.delta_y_norm_over_c;
+	    }
+
+	    void mpcReceiveRequestCallback(
+		   const mpc_dummy::OptimizationParameters::ConstPtr& msg)
+	    {
+		params = *msg;
+	    }	    
+
+	    void solveAndPublish(ros::Publisher &publisher) 
+            {
+	        updateInputData(); /* get input data */
+                rosenbrockSolverStatus status = solve();  /* solve!  */
+                updateResults(status); /* pack results into `results` */
+                publishToTopic(publisher);
+            }
+	}; /* end of class OptimizationEngineManager */
+
+
+} /* end of namespace mpc_dummy */
 
 
 
@@ -86,36 +117,29 @@ class OptimizationEngineManager {
  */
 int main(int argc, char **argv)
 {  
-  OptimizationEngineManager mng;
+  mpc_dummy::OptimizationEngineManager mng;
   ros::init(argc, argv, MPC_DUMMY_NODE_NAME);
   ros::NodeHandle n(MPC_DUMMY_BASE_TOPIC);
 
   ros::Publisher mpc_pub 
-      = n.advertise<mpc_dummy::OptimisationResult>(
-                  MPC_DUMMY_SOLUTION_TOPIC, 1000);
+      = n.advertise<mpc_dummy::OptimizationResult>(
+             MPC_DUMMY_SOLUTION_TOPIC,
+             MPC_DUMMY_SOLUTION_TOPIC_QUEUE_SIZE);
   ros::Subscriber sub 
       = n.subscribe(
-                  MPC_DUMMY_PARAMS_TOPIC, 1000, 
-                  &OptimizationEngineManager::mpcReceiveRequestCallback,
-                  &mng);
-  ros::Rate loop_rate(MPC_DUMMY_RATE);
-  
+             MPC_DUMMY_PARAMS_TOPIC,
+             MPC_DUMMY_PARAMS_TOPIC_QUEUE_SIZE, 
+             &mpc_dummy::OptimizationEngineManager::mpcReceiveRequestCallback,
+             &mng);
+  ros::Rate loop_rate(MPC_DUMMY_RATE);  /*TODO: Make MPC rate a parameter? */
  
-  int count = 0;
   while (ros::ok())
   {
-
-    mng.updateInputData();
-    rosenbrockSolverStatus status = mng.solve();  /* solve!  */
-    mng.updateResults(status); /* pack results into `results` */
-    mng.publishToTopic(mpc_pub);
+    mng.solveAndPublish(mpc_pub);
 
     ros::spinOnce();
     loop_rate.sleep();
-    ++count;
   }
-
-
 
   return 0;
 }
