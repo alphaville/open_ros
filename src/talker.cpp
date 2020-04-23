0e2f7752ab98df497a9c387f39eeed90ebe5e8e5
Pantelis Sopasakis <p.sopasakis@gmail.com>
2020-04-23 13:49:40 +0100

    impl OptimizationEngineManager

--- src/talker.cpp
+++ src/talker.cpp
@@ -13,59 +13,91 @@
 #define MPC_DUMMY_RATE 10
 
 
-class TalkerManager {
-    public:
+class OptimizationEngineManager {
+    private:
         mpc_dummy::OptimisationParameters params;
         mpc_dummy::OptimisationResult results;
         double p[ROSENBROCK_NUM_PARAMETERS] = {0};
         double u[ROSENBROCK_NUM_DECISION_VARIABLES] = {0};
+        rosenbrockCache *cache;
+        double init_penalty = 15.0;
 
-    TalkerManager() {
+    public:
+    OptimizationEngineManager() {
+        cache = rosenbrock_new();
+    }
+
+    ~OptimizationEngineManager() {
+        rosenbrock_free(cache);
+    }
+
+    /**
+     * updateResults 
+     */
+    void updateResults(rosenbrockSolverStatus& status) 
+    {
+        std::vector<double> sol(u, u+ROSENBROCK_NUM_DECISION_VARIABLES); 
+        results.solution = sol;
+        std::vector<double> y(status.lagrange, status.lagrange+ROSENBROCK_N1);
+        results.lagrange_multipliers = y;
+        results.inner_iterations = status.num_inner_iterations;
+        results.outer_iterations = status.num_outer_iterations;
+        results.norm_fpr = status.last_problem_norm_fpr;
+        results.penalty = status.penalty;
+        results.status = (int) status.exit_status;
+    }
+
+    void mpcReceiveRequestCallback(
+           const mpc_dummy::OptimisationParameters::ConstPtr& msg)
+    {
+        params = *msg;
+    }
+
+    void publishToTopic(ros::Publisher &publisher) 
+    {
+        publisher.publish(results);
+    }
+
+
+    void updateInputData() {
+        if (params.parameter.size() > 0) 
+        {
+	    for (int i=0; i<ROSENBROCK_NUM_PARAMETERS; ++i) 
+              p[i] = params.parameter[i];
+        }
+
+        if (params.initial_guess.size() == ROSENBROCK_NUM_DECISION_VARIABLES) 
+        {
+	    for (int i=0; i<ROSENBROCK_NUM_DECISION_VARIABLES; ++i) 
+                u[i] = params.initial_guess[i];
+        }
+    }
+
+    rosenbrockSolverStatus solve() {
+        return rosenbrock_solve(cache, u, p, 0, &init_penalty);
     }
 };
 
 
-static TalkerManager mng;
-
-
-void mpcReceiveRequestCallback(const mpc_dummy::OptimisationParameters::ConstPtr& msg)
-{
-  mng.params = *msg;
-}
-
-
-/**
- * updateResults 
- */
-void updateResults(rosenbrockSolverStatus& status) 
-{
-    std::vector<double> sol(mng.u, mng.u+ROSENBROCK_NUM_DECISION_VARIABLES); 
-    mng.results.solution = sol;
-    std::vector<double> y(status.lagrange, status.lagrange+ROSENBROCK_N1);
-    mng.results.lagrange_multipliers = y;
-    mng.results.inner_iterations = status.num_inner_iterations;
-    mng.results.outer_iterations = status.num_outer_iterations;
-    mng.results.norm_fpr = status.last_problem_norm_fpr;
-    mng.results.penalty = status.penalty;
-    mng.results.status = (int) status.exit_status;
-}
 
 
 /**
  * Main method
  */
 int main(int argc, char **argv)
-{
-  double init_penalty = 15.0;
-  rosenbrockCache *cache = rosenbrock_new();
-
+{  
+  OptimizationEngineManager mng;
   ros::init(argc, argv, MPC_DUMMY_NODE_NAME);
   ros::NodeHandle n(MPC_DUMMY_BASE_TOPIC);
 
   ros::Publisher mpc_pub 
-      = n.advertise<mpc_dummy::OptimisationResult>(MPC_DUMMY_SOLUTION_TOPIC, 1000);
+      = n.advertise<mpc_dummy::OptimisationResult>(
+                  MPC_DUMMY_SOLUTION_TOPIC, 1000);
   ros::Subscriber sub 
-      = n.subscribe(MPC_DUMMY_PARAMS_TOPIC, 1000, mpcReceiveRequestCallback);
+      = n.subscribe(
+                  MPC_DUMMY_PARAMS_TOPIC, 1000, 
+                  &OptimizationEngineManager::mpcReceiveRequestCallback,
+                  &mng);
   ros::Rate loop_rate(MPC_DUMMY_RATE);
   
  
@@ -73,29 +105,16 @@ int main(int argc, char **argv)
   while (ros::ok())
   {
 
-    if (mng.params.parameter.size() > 0) 
-    {
-	for (int i=0; i<ROSENBROCK_NUM_PARAMETERS; ++i) mng.p[i] = mng.params.parameter[i];
-    }
-
-    if (mng.params.initial_guess.size() == ROSENBROCK_NUM_DECISION_VARIABLES) 
-    {
-	for (int i=0; i<ROSENBROCK_NUM_DECISION_VARIABLES; ++i) 
-            mng.u[i] = mng.params.initial_guess[i];
-    }
-
-    rosenbrockSolverStatus status 
-       = rosenbrock_solve(cache, mng.u, mng.p, 0, &init_penalty);  /* solve!  */
-
-    updateResults(status); /* pack results into `results` */
-    mpc_pub.publish(mng.results); /* publish results (on /mpc/open_solution )*/
+    mng.updateInputData();
+    rosenbrockSolverStatus status = mng.solve();  /* solve!  */
+    mng.updateResults(status); /* pack results into `results` */
+    mng.publishToTopic(mpc_pub);
 
     ros::spinOnce();
     loop_rate.sleep();
     ++count;
   }
 
-  rosenbrock_free(cache);
 
 
   return 0;
